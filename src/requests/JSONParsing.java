3f3dd4a27ba51340dfa23b71f61c31fbca5672fd
Thomas Sammons <t.sammons@hotmail.com>
2015-03-28 16:04:40 +0000

    method tom

--- src/requests/JSONParsing.java
+++ src/requests/JSONParsing.java
@@ -3,11 +3,22 @@ package requests;
 import org.json.JSONObject;
 
 public class JSONParsing {
-
+	private JSONObject jsonobj = new JSONObject();
+	
 	public JSONParsing(){
 
 		
 		JSONObject o = new JSONObject();
+	}
+	
+	public static JSONObject getJson(String url, String resultCount){
+		
+		String requestString = "http://api.majestic.com/api/json?app_api_key=A1491C07BAB12F365BA1E073C69200DF&"
+				+ "cmd=GetRefDomains&item0="+url+"&Count="+resultCount+"&datasource=fresh";
+		
+		
+		
+		return ;
 	}
 	
 }
