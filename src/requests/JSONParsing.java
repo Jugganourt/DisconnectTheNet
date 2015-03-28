5f48a38c5fa2b3f052c47e533c6e4128718c69fd
Thomas Sammons <t.sammons@hotmail.com>
2015-03-28 16:37:36 +0000

    neat

--- src/requests/JSONParsing.java
+++ src/requests/JSONParsing.java
@@ -28,43 +28,24 @@ public class JSONParsing {
 	public static JSONObject getJson(String url, String resultCount){
 		JSONObject jsonobj = new JSONObject();
 		String requestString = "http://api.majestic.com/api/json?app_api_key=A1491C07BAB12F365BA1E073C69200DF&"
-				+ "cmd=GetRefDomains&item0="+url+"&Count="+resultCount+"&datasource=fresh";
-		 		
+				+ "cmd=GetRefDomains&item0="+url+"&Count="+resultCount+"&datasource=fresh";	
 		try {
 			URL obj = new URL(requestString);
-		
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-	 
-			// optional default is GET
 			con.setRequestMethod("GET");
-	 
-			//add request header
-			//con.setRequestProperty("User-Agent", USER_AGENT);
-	 
-			int responseCode = con.getResponseCode();
-			//System.out.println("\nSending 'GET' request to URL : " + requestString);
-			//System.out.println("Response Code : " + responseCode);
-	 
-			BufferedReader in = new BufferedReader(
-			        new InputStreamReader(con.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
-	 
 			while ((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
 			}
 			in.close();
-			jsonobj = new JSONObject(response.toString()); 
-						
+			jsonobj = new JSONObject(response.toString()); 		
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 		return jsonobj;
 	}
 	
