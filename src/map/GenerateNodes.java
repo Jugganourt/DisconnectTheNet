cf3a2bb59cd78628b236c7f6714f1a878c032751
Joseph Shaw <j.g.shaw95@gmail.com>
2015-03-28 20:43:20 +0000

    yes

--- src/map/GenerateNodes.java
+++ src/map/GenerateNodes.java
@@ -2,6 +2,7 @@ package map;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeMap;
 
@@ -12,19 +13,38 @@ public class GenerateNodes {
 	Map<String, Node> urlToNode = new TreeMap<String, Node>();
 	Set<Link> linksFound = new HashSet<Link>();
 
+	private final int upperChance = 100;
+	private final int linkChance = 20;
+	
+	
 	public GenerateNodes(String url, int numResults) {
 
 		GetLinks start = new GetLinks(url, numResults);
+		
+		System.out.println(start.getField("Domain").size());
+		
+		Random r = new Random();
 
 		for (String site : start.getField("Domain")) {
-			linksFound.add(new Link(url, site));
+			if (r.nextInt(upperChance) < linkChance)
+				linksFound.add(new Link(url, site));
 		}
 
 		Node temp = new Node(url);
 		urlToNode.put(url, temp);
 
-		Link l = new Link("wisc.edu", "mit.edu");
-		linksFound.add(l);
+		for (String site : start.getField("Domain")) {
+			System.out.println("Loop: " + site);
+			GetLinks siteLinks = new GetLinks(site, numResults);
+
+			for (String site2 : siteLinks.getField("Domain")) {
+				if (r.nextInt(upperChance) < linkChance)
+					linksFound.add(new Link(site, site2));
+			}
+
+			temp = new Node(url);
+			urlToNode.put(url, temp);
+		}
 
 		for (Link link : linksFound) {
 			System.out.println(link);
@@ -33,7 +53,7 @@ public class GenerateNodes {
 	}
 
 	public static void main(String[] args) {
-		new GenerateNodes("mit.edu", 100);
+		new GenerateNodes("mit.edu", 200);
 
 	}
 
